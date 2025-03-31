2925ce4502a7d585c4eafc6220409b275599fcf4
Barros <barrosns2603@gmail.com>
2025-03-31 01:00:55 -0300

    relatorio de compras

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿﻿using Microsoft.Extensions.Logging;
+using MauiAppMinhasCompras.Helpers;
 
 namespace MauiAppMinhasCompras
 {
@@ -18,6 +19,9 @@ namespace MauiAppMinhasCompras
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "minhascompras.db");
+
+            builder.Services.AddSingleton(new SQLiteDatabaseHelper(dbPath));
 
             return builder.Build();
         }
